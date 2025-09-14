11e1aefe486f557ab604562b3bd9018faa0a8277
ajay <ajayem1222@gmail.com>
2025-09-14 21:35:24 +0530

    update

---
+++ Leetcode/Solution3.java
@@ -0,0 +1,17 @@
+package Leetcode;
+
+//! 3683. Earliest Time to Finish One Task
+
+public class Solution3 {
+    int n = tasks.length;
+        int m = tasks[0].length;
+        int val = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=0;j<m;j++){
+                sum += tasks[i][j];
+            }
+            val = Math.min(val,sum);
+        }
+        return val;
+}
