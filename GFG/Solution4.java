28aa95bb3b93a095b88e782a0d03f9eb078295b2
ajay <ajayem1222@gmail.com>
2025-09-17 21:30:05 +0530

    updated

---
+++ GFG/Solution4.java
@@ -0,0 +1,28 @@
+import java.util.Stack;
+//! valid parenthesis
+public class Solution4 {
+    public boolean isBalanced(String s) {
+        // code here
+        Stack<Character> st = new Stack<>();
+        int n = s.length();
+        for(int i=0;i<n;i++){
+            char ch =s.charAt(i);
+            if(st.isEmpty()){
+                st.push(ch);
+            }
+            else if(st.peek()=='(' && ch==')'){
+                st.pop();
+            }
+            else if(st.peek()=='[' && ch==']'){
+                st.pop();
+            }
+            else if(st.peek()=='{' && ch=='}'){
+                st.pop();
+            }
+            else{
+                st.push(ch);
+            }
+        }
+        return st.isEmpty();
+    }
+}
