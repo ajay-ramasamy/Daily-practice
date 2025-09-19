7dbb7be4c96fe7b7be1ab8cea847e83138f4e5df
ajay <ajayem1222@gmail.com>
2025-09-19 22:48:20 +0530

    updated

---
+++ Leetcode/Solution9.java
@@ -0,0 +1,23 @@
+//! 696. Count Binary Substrings
+
+
+class Solution9 {
+    public int countBinarySubstrings(String s) {
+        int left = 0;
+        int right = 1;
+        int result =0;
+        int n = s.length();
+        for(int i=1; i<n;i++){
+            if(s.charAt(i-1)==s.charAt(i)){
+                right++;
+            }
+            else{
+                result += Math.min(left,right);
+                left = right;
+                right = 1;
+            }
+        }
+        result += Math.min(left,right);
+        return result;
+    }
+}
