983a869fb384940dc44bfdc7a1f3371b0162a06c
ajay ramasamy j <ajayem1222@gmail.com>
2025-09-25 23:04:30 +0530

    Created

---
+++ Leetcode/Solution11.java
@@ -0,0 +1,22 @@
+//! 746. Min Cost Climbing Stairs
+
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int dp[] = new int[n+1];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        if(n<3){
+            return Math.min(dp[n-1],dp[n-2]);
+        }
+        for(int i=2;i<n;i++){
+            dp[i] = Math.min(dp[i-1],dp[i-2])+cost[i];
+        }
+        for(int num:dp){
+            System.out.print(num+" ");
+        }
+        return Math.min(dp[n-1],dp[n-2]);
+    }
+
+    
+}
